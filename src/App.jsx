33c3e37871a7ab9848cc8051620699e2b5d5e5e6
JanaWalt <jana.plotkina@googlemail.com>
2025-11-03 20:55:34 +0100

    feat: add base input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-        <Button />
+      <Button />
+      <Input />
       <h1>Vite + React</h1>
       <div className="card">
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      
       </div>
-      
     </>
   );
 }
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Input.module.css";
+
+const Input = () => {
+  return (
+    <div>
+      <input className={styles.input} type="text" />
+    </div>
+  );
+};
+
+export default Input;
